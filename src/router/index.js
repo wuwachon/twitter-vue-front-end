88e79a339db375558de578cb8ea1cf0f3ef62c70
Mei Mei <>
2022-07-31 19:11:14 +0800

    feat: add AccountingSetting layout

--- src/router/index.js
+++ src/router/index.js
@@ -39,9 +39,14 @@ const routes = [
     component: () => import('../views/AdminLogin.vue')
   },
   {
-    path: '/user',
+    path: '/user/:id',
     name: 'user',
     component: () => import('../views/User.vue')
+  },
+  {
+    path: '/setting',
+    name: 'account-setting',
+    component: () => import('../views/AccountSetting.vue')
   },
   {
     path: '*',
