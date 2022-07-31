32156b7a2f884df960137e52020527202f7a3fbb
Mei Mei <>
2022-07-31 19:09:21 +0800

    fix: Use Vuex attribute state and mutations

---
+++ src/store/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+Vue.config.devtools = true
+
+export const store = new Vuex.Store({
+  state: {
+    counter: 0
+  }
+})
+
+export default new Vuex.Store({
+  // data
+  state: {
+    currentUser: {
+      id: -1,
+      name: '',
+      email: '',
+      image: '',
+      isAdmin: false
+    },
+    isAuthenticated: false
+  },
+  // 
+  getters: {
+  },
+  mutations: {
+    setCurrentUser (state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        // 將 API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
+        ...currentUser
+      }
+      // 將使用者的登入狀態改為 true
+      state.isAuthenticated = true
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
